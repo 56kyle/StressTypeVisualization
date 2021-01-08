05df7915dadd7864e61c2334ed0e3ba85132e7cc
Kyle Oliver <oliverke@mail.uc.edu>
2021-01-07 20:39:56 -0500

    Updated and fixed quite a bit. Made sure to remove any outside sources aswell now that this is public

--- PluginMain.cs
+++ PluginMain.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Linq;
 using System.Reflection;
-using PolyTechFramework;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
@@ -14,77 +15,197 @@ using UnityEngine;
 using UnityEngine.UI;
 using PolyPhysics;
 using Poly.Physics.Solver;
-using Rewired.Demos;
-using Vectrosity;
 
 namespace StressTypeVisualization
 {
-    [BepInPlugin("polytech.stress_type_visualization", "Stress Type Visualization", "1.2.0")]
-	[BepInDependency("polytech.polytechframework", "0.7.5")]
+    [BepInPlugin("56kyle.stress_type_visualization", "Stress Type Visualization", "1.5.0")]
 	[BepInProcess("Poly Bridge 2.exe")]
-    public class PluginMain : PolyTechMod
+    public class PluginMain : BaseUnityPlugin
     {
-        public new const string PluginGuid = "polytech.stress_type_visualization";
-        public new const string PluginName = "Stress Type Visualization";
-        public new const string PluginVersion = "1.2.0";
         private static ConfigEntry<bool> configIsEnabled;
+
+        private static ConfigEntry<bool> configStressViewIsEnabled;
+        private static ConfigEntry<bool> configTensileCompressiveModeIsEnabled;
+
+        private static ConfigEntry<Color> configTensileMin;
+        private static ConfigEntry<Color> configTensileMax;
+        private static ConfigEntry<Color> configCompressiveMin;
+        private static ConfigEntry<Color> configCompressiveMax;
+
+        private static ConfigEntry<bool> configThresholdModeIsEnabled;
+
+        private static ConfigEntry<float> configThresholdModeMinStress;
+        private static ConfigEntry<float> configThresholdModeMaxStress;
+
+        private static ConfigEntry<bool> configPausingIsEnabled;
+
         private static ConfigEntry<bool> configPauseBeforeBreak;
+
         private static ConfigEntry<bool> configEveryBreak;
+
         private static ConfigEntry<bool> configHighlightFirstBreak;
 
-        public static bool pauseBeforeBreak = true;
-        public static bool highlightFirstBreak = true;
-        public static bool everyBreak = false;
+        public static int colorId = Shader.PropertyToID("_Color");
+        public static int stressId = Shader.PropertyToID("_Stress");
+
+        public static Color minTensileColor;
+        public static Color maxTensileColor;
+
+        public static Color minCompressiveColor;
+        public static Color maxCompressiveColor;
+        public static bool disabled = false;
+
 
         public PluginMain()
         {
-            configIsEnabled = base.Config.Bind("General", "Enabled", true, "Enables the mod");
-            configPauseBeforeBreak = base.Config.Bind("General", "Pause Before First Break", true, "Makes it so the game will be paused right before the first break occurs.");
-            configEveryBreak = base.Config.Bind("General", "Pause Before Every Break", false, "Pauses the game before a break occurs.");
-            configHighlightFirstBreak = base.Config.Bind("General", "Highlight First Break", true, "Highlights the first breakage immediately as opposed to when inside build mode immediately as opposed to when inside build mode");
+            // Fallback options
+            Color defaultTensMin = Color.green;
+            Color defaultCompMin = Color.green;
+            Color defaultTensMax = Color.red;
+            Color defaultCompMax = Color.blue;
+
+            // Normal stress color and the same skew amount for the shade of blue
+            ColorUtility.TryParseHtmlString("#4D993D", out defaultTensMin);
+            ColorUtility.TryParseHtmlString("#4D993D", out defaultCompMin);
+            ColorUtility.TryParseHtmlString("#B33636", out defaultTensMax);
+            ColorUtility.TryParseHtmlString("#4A36B3", out defaultCompMax);
+
+            // Enabling the mod
+            configIsEnabled = Config.Bind("General", "Enabled", true,
+                new ConfigDescription("Enables the mod", null, new ConfigurationManagerAttributes{Order = 1}));
+
+            // Tensile and Compressive Mode
+            configTensileCompressiveModeIsEnabled = Config.Bind("General.Differentiating Stress", "Enabled", true,
+                new ConfigDescription("Enables stress differentiation mode", null, new ConfigurationManagerAttributes{Order = 5}));
+
+            configTensileMin = Config.Bind("General.Differentiating Stress", "Tensile Min Stress Color", defaultTensMin,
+                new ConfigDescription("The color that will show for max tensile stress", null, new ConfigurationManagerAttributes{Order = 4}));
+            configTensileMax = Config.Bind("General.Differentiating Stress", "Tensile Max Stress Color", defaultTensMax,
+                new ConfigDescription("The color that will show for max tensile stress", null, new ConfigurationManagerAttributes{Order = 3}));
+
+            configCompressiveMin = Config.Bind("General.Differentiating Stress", "Compressive Min Stress Color", defaultCompMin,
+                new ConfigDescription("The color that will show for the min tensile stress", null, new ConfigurationManagerAttributes{Order = 2}));
+            configCompressiveMax = Config.Bind("General.Differentiating Stress", "Compressive Max Stress Color", defaultCompMax,
+                new ConfigDescription("The color that will show for max tensile stress", null, new ConfigurationManagerAttributes{Order = 1}));
+
+            // Threshold mode
+            configThresholdModeIsEnabled = Config.Bind("General.Threshold Mode", "Enabled", false,
+                new ConfigDescription("Enables Threshold Mode", null, new ConfigurationManagerAttributes{Order = 2}));
+            configThresholdModeMinStress = Config.Bind("General.Threshold Mode", "Min Stress", 0.4f,
+                new ConfigDescription("The minimum stress before changes are made", new AcceptableValueRange<float>(0f, 1f), new ConfigurationManagerAttributes{Order = 1}));
+
+            // Pausing options
+            configPausingIsEnabled = Config.Bind("General.Stop Simulation On Break", "Enabled", false,
+                new ConfigDescription("Enables the pausing options", null, new ConfigurationManagerAttributes{Order = 4}));
+            configPauseBeforeBreak = Config.Bind("General.Stop Simulation On Break", "Pause Before First Break", false,
+                new ConfigDescription("Makes it so the game will be paused right before the first break occurs.", null, new ConfigurationManagerAttributes{Order = 3}));
+            configEveryBreak = Config.Bind("General.Stop Simulation On Break", "Pause Before Every Break", false,
+                new ConfigDescription("Pauses the game before a break occurs.", null, new ConfigurationManagerAttributes{Order = 3}));
+            configHighlightFirstBreak = Config.Bind("General.Stop Simulation On Break", "Highlight First Break", false,
+                new ConfigDescription("Highlights the first breakage immediately as opposed to when inside build mode (Not always accurate)", null, new ConfigurationManagerAttributes{Order = 2}));
         }
 
         private void Awake()
         {
             PluginMain.staticLogger = base.Logger;
-			new Harmony("polytech.stress_type_visualization").PatchAll(Assembly.GetExecutingAssembly());
-            this.setSettings(this.getSettings());
-            PolyTechMain.registerMod(this);
+			new Harmony("56kyle.stress_type_visualization").PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        public virtual void enableMod()
+        public void RefreshSettings()
         {
-            this.isEnabled = true;
-            configIsEnabled.Value = true;
-        }
+            minTensileColor = configTensileMin.Value;
+            maxTensileColor = configTensileMax.Value;
+            minCompressiveColor = configCompressiveMin.Value;
+            maxCompressiveColor = configCompressiveMax.Value;
+            if (!configIsEnabled.Value)
+            {
+                configTensileCompressiveModeIsEnabled.Value = false;
+                configPausingIsEnabled.Value = false;
+                disabled = true;
+            }
 
-        public virtual void disableMod()
-        {
-            this.isEnabled = false;
-            configIsEnabled.Value = false;
-        }
-
-        public override void setSettings(string settings)
-        {
-            this.isEnabled = PluginMain.configIsEnabled.Value;
-            pauseBeforeBreak = PluginMain.configPauseBeforeBreak.Value;
-            highlightFirstBreak = PluginMain.configHighlightFirstBreak.Value;
-            everyBreak = PluginMain.configEveryBreak.Value;
-        }
-
-        public override string getSettings()
-        {
-            return base.Config.ToString();
+            if (!configPausingIsEnabled.Value)
+            {
+                configPauseBeforeBreak.Value = false;
+                configEveryBreak.Value = false;
+                configHighlightFirstBreak.Value = false;
+            }
         }
 
         public void Update()
         {
-            this.setSettings("");
+            RefreshSettings();
         }
 
         private static ManualLogSource staticLogger;
         public static bool firstBreakWasFound = false;
         public static GameState m_GameState;
+        public static Dictionary<int, List<Material>> originMaterials = new Dictionary<int, List<Material>>();
+
+        private static void SetShaderColor(BridgeEdge bridgeEdge, float stressNormalized, float stressNormalizedSigned)
+        {
+            Color minColor;
+            Color maxColor;
+
+            if (stressNormalizedSigned <= 0.0f)
+            {
+                minColor = minTensileColor;
+                maxColor = maxTensileColor;
+            }
+            else
+            {
+                minColor = minCompressiveColor;
+                maxColor = maxCompressiveColor;
+            }
+
+            if (minColor == null || maxColor == null) { return; }
+
+            Color desiredColor = Color.Lerp(minColor, maxColor, stressNormalized);
+
+            if (bridgeEdge.m_MeshRenderer.material.HasProperty(stressId))
+            {
+                originMaterials[bridgeEdge.GetHashCode()] = new List<Material>{bridgeEdge.m_MeshRenderer.material, bridgeEdge.m_MeshRenderer.sharedMaterial};
+                bridgeEdge.m_MeshRenderer.material = new Material(Shader.Find("Standard"));
+                bridgeEdge.m_MeshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+            }
+            bridgeEdge.m_MeshRenderer.material.color = desiredColor;
+            bridgeEdge.m_MeshRenderer.sharedMaterial.color = desiredColor;
+        }
+
+        public static void CheckMaterial(BridgeEdge bridgeEdge)
+        {
+            if (!bridgeEdge.m_MeshRenderer.material.HasProperty(stressId))
+            {
+                Material originalMaterial = null;
+                Material originalSharedMaterial = null;
+                try
+                {
+                    int hashCode = bridgeEdge.GetHashCode();
+                    List<Material> originalMaterialList = originMaterials[hashCode];
+                    if (originalMaterialList != null)
+                    {
+                        originalMaterial = originalMaterialList[0];
+                        originalSharedMaterial = originalMaterialList[1];
+                    }
+
+                    originMaterials.Remove(hashCode);
+                }
+                catch (KeyNotFoundException)
+                {
+                    staticLogger.LogMessage("Edge has no original material, assuming it's fine");
+                }
+
+                if (originalMaterial != null)
+                {
+                    bridgeEdge.m_MeshRenderer.material = originalMaterial;
+                }
+
+                if (originalSharedMaterial != null)
+                {
+                    bridgeEdge.m_MeshRenderer.sharedMaterial = originalSharedMaterial;
+                }
+            }
+        }
 
         // Adjusts the stress color
         [HarmonyPatch(typeof(BridgeEdge), "SetStressColor")]
@@ -93,29 +214,38 @@ namespace StressTypeVisualization
             [HarmonyPostfix]
             private static void Postfix(ref BridgeEdge __instance, ref float stressNormalized, ref MaterialPropertyBlock ___m_MaterialPropertyBlock, ref int ___m_ShaderIDForStress, ref int ___m_ShaderIDForColorBlind)
             {
-                if (PluginMain.configIsEnabled.Value)
+
+                if (stressNormalized == 0f)
                 {
-                    if (stressNormalized == 0f)
-                    {
-                        return;
-                    }
+                    CheckMaterial(__instance);
+                    return;
+                }
+
+                float alteredStress;
+                if (configThresholdModeIsEnabled.Value)
+                {
+                    alteredStress = stressNormalized >= configThresholdModeMinStress.Value ? 1.0f : stressNormalized;
+                }
+                else
+                {
+                    alteredStress = stressNormalized;
+                }
+
+                if (configTensileCompressiveModeIsEnabled.Value)
+                {
                     if (__instance.m_PhysicsEdge)
                     {
                         Edge edge = __instance.m_PhysicsEdge;
                         if (edge.handle)
                         {
-                            ___m_MaterialPropertyBlock.SetFloat(___m_ShaderIDForStress, stressNormalized);
-                            if (edge.handle.stressNormalizedSigned < 0)
-                            {
-                                ___m_MaterialPropertyBlock.SetFloat(___m_ShaderIDForColorBlind, Profile.m_ColorBlindModeOn ? 1f : 0f);
-                            }
-                            else
-                            {
-                                ___m_MaterialPropertyBlock.SetFloat(___m_ShaderIDForColorBlind, Profile.m_ColorBlindModeOn ? 0f : 1f);
-                            }
-                            __instance.m_MeshRenderer.SetPropertyBlock(___m_MaterialPropertyBlock);
+                            PluginMain.SetShaderColor(__instance, alteredStress, edge.handle.stressNormalizedSigned);
                         }
                     }
+                }
+                else
+                {
+                    ___m_MaterialPropertyBlock.SetFloat(___m_ShaderIDForColorBlind, Profile.m_ColorBlindModeOn ? 1f : 0f);
+                    __instance.m_MeshRenderer.SetPropertyBlock(___m_MaterialPropertyBlock);
                 }
             }
         }
@@ -128,13 +258,13 @@ namespace StressTypeVisualization
             private static void Postfix(float __result)
             {
                 // __result will be equal to the max stress that was present.
-                if (!configIsEnabled.Value)
+                if (!configPausingIsEnabled.Value)
                 {
                     return;
                 }
                 if (__result == 1f)
                 {
-                    if ((!firstBreakWasFound) || everyBreak)
+                    if ((!firstBreakWasFound) || configEveryBreak.Value)
                     {
                         PauseGame();
                         firstBreakWasFound = true;
@@ -157,7 +287,7 @@ namespace StressTypeVisualization
                                 }
                             }
                         }
-                        if (highlightFirstBreak)
+                        if (configHighlightFirstBreak.Value)
                         {
                             if (mostLikelyBroken != null)
                             {
@@ -168,6 +298,7 @@ namespace StressTypeVisualization
                 }
             }
 
+            // Just an ease of use function for pausing the game in a way that updates the UI
             private static void PauseGame()
             {
                 GameStateSim.Pause();
@@ -194,6 +325,7 @@ namespace StressTypeVisualization
                     if (m_GameState != GameState.SIM)
                     {
                         firstBreakWasFound = false;
+                        originMaterials.Clear();
                     }
                 }
             }
